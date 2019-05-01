1c80c2154cb0f77c43e9b52c2d0398a320db9200
baby7305 <songhaibei7305@163.com>
2019-05-01 23:50:49 +0800

    重写doGet方法

--- src/main/java/com/example/demo/web/servlet/MyServlet.java
+++ src/main/java/com/example/demo/web/servlet/MyServlet.java
@@ -1,4 +1,15 @@
 package com.example.demo.web.servlet;
 
-public class MyServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class MyServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().println("Hello,World");
+	}
 }
