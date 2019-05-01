08d9396a2dc8f2049f4202b6d4e95d8cd00f2839
baby7305 <songhaibei7305@163.com>
2019-05-01 23:53:04 +0800

    配置doGet方法映射路径

--- src/main/java/com/example/demo/web/servlet/MyServlet.java
+++ src/main/java/com/example/demo/web/servlet/MyServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet
+@WebServlet(urlPatterns = "/my/servlet")
 public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.getWriter().println("Hello,World");
