f478ea4aea0980bb374207dbf7067bdd5b6e9fd6
baby7305 <songhaibei7305@163.com>
2019-05-02 11:12:50 +0800

    调整支持异步

--- src/main/java/com/example/demo/web/servlet/MyServlet.java
+++ src/main/java/com/example/demo/web/servlet/MyServlet.java
@@ -1,5 +1,6 @@
 package com.example.demo.web.servlet;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,9 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/my/servlet")
+@WebServlet(urlPatterns = "/my/servlet", asyncSupported = true)
 public class MyServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println("Hello,World");
+		AsyncContext asyncContext = req.startAsync();
+		asyncContext.start(() -> {
+			try {
+				resp.getWriter().println("Hello,World");
+				// 触发完成
+				asyncContext.complete();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
